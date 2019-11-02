667fd368728dad58f38b56f3c5d63db0cbcf2039
petescode <mrpmwood@gmail.com>
2019-11-02 14:19:09 -0700

    added FIPS check

--- easy-verify-hash.ps1
+++ easy-verify-hash.ps1
@@ -7,23 +7,37 @@ Notes:
 
 Development:
 - FIPS enabled/disabled? Affects which algorithms will work
+- Must work in contrained lanugage mode
 #>
 
 function Get-Algorithm {
-    # since we have already gotten the user used to selecting from an array of objects,
-    # let's keep going with that theme
-
     # zeroize $selection as it's been used by the time this function is called
     [int]$selection=0
-
-    # this method of array building with custom objects is supported as far back as PowerShell 3
-    $available_algorithms=@(
+    
+    # check for FIPS here, modify menu options based on result
+    Try{
+        # this block executes if no error - meaning FIPS is not enabled
+        New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider | Out-Null
+        
+        # this method of array building with custom objects is supported as far back as PowerShell 3
+        $available_algorithms=@(
         [PSCustomObject]@{Number = 1; Algorithm = "MD5"},
         [PSCustomObject]@{Number = 2; Algorithm = "SHA1"},
         [PSCustomObject]@{Number = 3; Algorithm = "SHA256"},
         [PSCustomObject]@{Number = 4; Algorithm = "SHA384"},
         [PSCustomObject]@{Number = 5; Algorithm = "SHA512"}
-    )
+        )
+    }
+    Catch{
+        # this block executes if the Try{} block had an error - meaning FIPS is enabled
+
+        $available_algorithms=@(
+        [PSCustomObject]@{Number = 1; Algorithm = "SHA1"}, # need to test this
+        [PSCustomObject]@{Number = 2; Algorithm = "SHA256"},
+        [PSCustomObject]@{Number = 3; Algorithm = "SHA384"},
+        [PSCustomObject]@{Number = 4; Algorithm = "SHA512"}
+        )
+    }
     $available_algorithms | Out-Host
 
     Do{
@@ -39,21 +53,23 @@ function Get-FIPSAlgorithm {
     
 }
 
+# default path unless user switches it later
+$path = "C:\Users"
 
 # this needs to be a function so we can call it later during switch
 Clear-Host
-[string]$name = Read-Host "Enter name of file to search for"
-$results = Get-ChildItem -Path C:\Users -Recurse -File -Include "*$name*" -ErrorAction SilentlyContinue | Select Length,Name,FullName
+[string]$name = Read-Host "`nEnter name of file to search for"
+$results = Get-ChildItem -Path $path -Recurse -File -Include "*$name*" -ErrorAction SilentlyContinue | Select-Object Length,Name,FullName
 #$results | Out-Host
 
 # this while loop only kicks in if there are no files found by the entered name at the default path
 while($NULL -eq $results){
     Clear-Host
-    Write-Host "Searched for: '$name'"
+    Write-Host "`nSearched for: '$name'"
     Write-Host "`nNo file found!`n"
     Write-Host "What would you like to do?"
-    Write-Host "1. Try again"
-    Write-Host "2. Refine search"
+    Write-Host "1. Change search name"
+    Write-Host "2. Change search path"
     Write-Host "Q. Quit"
 
     $selection = Read-Host "`nChoose an option"
@@ -61,19 +77,20 @@ while($NULL -eq $results){
     switch($selection){
         1{ 
             Clear-Host
-            [string]$name = Read-Host "Enter name of file to search for"
-            $results = Get-ChildItem -Path C:\Users -Recurse -File -Include "*$name*" -ErrorAction SilentlyContinue | Select FullName
+            [string]$name = Read-Host "`nEnter name of file to search for"
+            $results = Get-ChildItem -Path $path -Recurse -File -Include "*$name*" -ErrorAction SilentlyContinue | Select-Object Length,Name,FullName
         }
         2{
-            $search_from = Read-Host "`nEnter search path to start from (default was C:\Users)"
+            $path = Read-Host "`nEnter search path to start from (default was C:\Users)"
             
             # verify the entered path is valid
-            while(-Not $(Test-Path $search_from)){
-                $search_from = Read-Host "`nEnter search path to start from (default was C:\Users)"
+            # try while $() -eq $FALSE or $NULL or something to fix entering nothing, THEN do this for testing if the path is actually good
+            while(-Not $(Test-Path $path)){
+                Write-Warning "'$path' does not exist! Try again"
+                $path = Read-Host "`nEnter search path to start from (default was C:\Users)"
             }
-
             # run the search again from new, validated search path
-            $results = Get-ChildItem -Path $search_from -Recurse -File -Include "*$name*" -ErrorAction SilentlyContinue | Select FullName
+            $results = Get-ChildItem -Path $path -Recurse -File -Include "*$name*" -ErrorAction SilentlyContinue | Select-Object Length,Name,FullName
         }
         q{ EXIT }
         default{ $NULL -eq $results } # start the loop over if one of these options is not selected
@@ -91,13 +108,14 @@ $results | ForEach-Object{
     $object | Add-Member -Type NoteProperty -Name "Name" -Value $(Split-Path -Path $_.FullName -Leaf)
     $object | Add-Member -Type NoteProperty -Name "FullName" -Value $_.FullName
     $object | Add-Member -Type NoteProperty -Name "Size" -Value $_.Length
+    $object | Add-Member -Type NoteProperty -Name "Path" -Value $(Split-Path -Path $_.FullName)
     $list += $object
 }
 
 # if only 1 file name match, auto-select it; else (multiple results) call function for menu
 
 if($list.count -gt 1){ # multiple files match the search; we must choose which one
-    $list | Select Number,Name | Out-Host
+    $list | Select-Object Number,Name | Out-Host
     [int]$selection=0
     Do{
         Try{ $selection = Read-Host "Select a file" }
@@ -107,11 +125,13 @@ if($list.count -gt 1){ # multiple files match the search; we must choose which o
     $file_name = $list[$selection-1].Name
     $file = $list[$selection-1].FullName
     $file_size_in_bytes = $list[$selection-1].Size
+    $file_path = $list[$selection-1].Path
 }
 else{ # only 1 result for search; this is our file
     $file_name = $list[0].Name
     $file = $list[0].FullName
     $file_size_in_bytes = $list[$selection-1].Size
+    $file_path = $list[$selection-1].Path
 }
 
 
@@ -122,7 +142,7 @@ if($file_size_in_bytes -ge 1073741824){
     Set-Variable -Name file_size -Value "$file_size_in_gb GB"
 }   
 elseif(($file_size_in_bytes -lt 1073741824) -and ($file_size_in_bytes -ge 1048576)){
-    # this should be in a MB
+    # this should be in MB
     $file_size_in_mb = [math]::Round($($file_size_in_bytes / 1024 / 1024),2)
     Set-Variable -Name file_size -Value "$file_size_in_mb MB"
 }
@@ -137,14 +157,18 @@ elseif($file_size_in_bytes -lt 1024){
 
 
 Clear-Host
-Write-Host "File selected is: " -NoNewline; Write-Host "$file_name" -ForegroundColor Cyan
+Write-Host "`nFile selected is: " -NoNewline; Write-Host "$file_name" -ForegroundColor Cyan
 Get-Algorithm
 
+# create a file info table for reporting purposes
+$file_info=@([PSCustomObject]@{Path = "$file_path"; File = "$file_name"; Size = "$file_size"; Algorithm = $algorithm})
+
 Clear-Host
-Write-Host "Calculating $algorithm hash for $file_name..." -NoNewline
-$true_hash = Get-FileHash -LiteralPath $file -Algorithm $algorithm | Select -Expand Hash
-Write-Host "done"
-# waiting message...size of file?
+$file_info | Format-List | Out-Host
+Write-Host "Computing hash..." -NoNewline
+$true_hash = Get-FileHash -LiteralPath $file -Algorithm $algorithm | Select-Object -Expand Hash
+Write-Host "done" -ForegroundColor Cyan
+
 
 [string]$given_hash = Read-Host "`nEnter hash given to you"
 
@@ -163,8 +187,6 @@ if($true_hash -eq $given_hash){
     Write-Host "______________________________________"
     Write-Host "Computed hash: $true_hash"
     Write-Host "Provided hash: $given_hash"
-    Write-Host "`n`nYour algorithm was: $algorithm"
-    Write-Host "Your file was: $file_name`n" 
 }
 else{ 
     Clear-Host
@@ -172,7 +194,6 @@ else{
     Write-Host "______________________________________"
     Write-Host "Computed hash: $true_hash"
     Write-Host "Provided hash: $given_hash"
-    Write-Host "`n`nYour algorithm was: $algorithm"
-    Write-Host "Your file was: $file_name`n" 
 }
+$file_info | Format-List | Out-Host
 #>
